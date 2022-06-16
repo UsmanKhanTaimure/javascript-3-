172dcc2e81e81daaaa9e67c83bf7e5df583c4d31
UsmanKhanTaimure <107642292+UsmanKhanTaimure@users.noreply.github.com>
2022-06-16 10:49:12 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// Chapter_3
+
+// Question_1
+// var age = 18;
+// alert(age)
+
+// Question_2
+// var str = "You Have Visited This Site ";
+// var num = 14 +" Time";
+// var full = str + num;
+// document.write(full)
+
+// Question_3
+// var birth = "My Birth year is ";
+// var num = 1990;
+// var full = birth + num;
+// document.write(full)
+
+// Question_4
+// var UserName = prompt("ENTER YOUR USERNAME");
+// var Quantity = prompt("ENTER YOUR QUANTITY");
+// var Product = prompt("ENTER YOUR PRODUCTS");
+// var up = UserName.toUpperCase("")
+// var upp = Quantity.toUpperCase("")
+// var uppe = Product.toUpperCase("")
+// document.write(up)
+// document.write(upp)
+// document.write(uppe)
